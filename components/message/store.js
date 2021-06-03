35c3d87a18aef68af3ec515094d920a99cd13365
JavierReivajGomez <jafg84alejandro@gmail.com>
2021-06-02 21:19:30 -0500

    vamos, tu puedes XD

--- components/message/store.js
+++ components/message/store.js
@@ -1,8 +1,17 @@
-const list = []
+const MongoClient = require('mongodb').MongoClient;
+const model = require("./model")
 
+const uri = "mongodb+srv://JavierReivajGomez:<password>@cluster0.ps4fu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 function addMessage (message) {
-    list.push(message);
+    const MyMessage = new Model (message);
+    MyMessage.save()
 }
 
 function getMessage() {
